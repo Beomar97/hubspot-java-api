4189ef7f9c79c067bdd3dfc8a08506e207146acc
Marco Forster <marco-forster@outlook.com>
2021-02-12 17:26:31 +0100

    feat: added listCompanies method with optional parameters to crm api
    listCompanies can now be called with or without optional parameters,
    optional parameters can be null

--- src/main/java/ch/marcoforster/libraries/hubspot/api/CrmApi.java
+++ src/main/java/ch/marcoforster/libraries/hubspot/api/CrmApi.java
@@ -5,11 +5,21 @@ import ch.marcoforster.libraries.hubspot.models.crm.companies.Company;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface CrmApi {
 
     @GET("crm/v3/objects/companies")
     Call<CompaniesByPaging> listCompanies();
+
+    @GET("crm/v3/objects/companies")
+    Call<CompaniesByPaging> listCompanies(
+            @Query("limit") Integer limit,
+            @Query("after") String after,
+            @Query("properties") String[] properties,
+            @Query("associations") String[] associations,
+            @Query("archived") Boolean archived
+    );
 
     @GET("crm/v3/objects/companies/{companyId}")
     Call<Company> readCompany(@Path("companyId") String companyId);
