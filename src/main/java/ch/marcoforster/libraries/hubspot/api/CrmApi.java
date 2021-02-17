80cc824e6adab1b12371cedfe922ac2b202e11b4
Marco Forster <marco-forster@outlook.com>
2021-02-17 10:36:39 +0100

    docs: added javadoc comments to CRM API
    added javadoc comments to existing methods in CRM API (CrmApi):
    listCompanies, createCompany, readCompany, updateCompany and
    archiveCompany

--- src/main/java/ch/marcoforster/libraries/hubspot/api/CrmApi.java
+++ src/main/java/ch/marcoforster/libraries/hubspot/api/CrmApi.java
@@ -8,9 +8,33 @@ import retrofit2.http.*;
 
 public interface CrmApi {
 
+    /**
+     * <p>
+     *     Read a page of companies.
+     * </p>
+     *
+     * @return  A list of companies by paging. {@link CompaniesByPaging}
+     * @see     <a href="https://developers.hubspot.com/docs/api/crm/companies">Companies</a>
+     * @since   0.0.1
+     */
     @GET("crm/v3/objects/companies")
     Call<CompaniesByPaging> listCompanies();
 
+    /**
+     * <p>
+     *     Read a page of companies with optional parameters.
+     *     Control what is returned via the properties query param.
+     * </p>
+     *
+     * @param limit         The maximum number of results to display per page ({@default 10}).
+     * @param after         The paging cursor token of the last successfully read resource will be returned as the paging.next.after JSON property of a paged response containing more results.
+     * @param properties    A comma separated list of the properties to be returned in the response. If any of the specified properties are not present on the requested object(s), they will be ignored.
+     * @param associations  A comma separated list of object types to retrieve associated IDs for. If any of the specified associations do not exist, they will be ignored.
+     * @param archived      Whether to return only results that have been archived ({@default false}).
+     * @return              A list of companies by paging {@link CompaniesByPaging}.
+     * @see                 <a href="https://developers.hubspot.com/docs/api/crm/companies">Companies</a>
+     * @since               0.0.1
+     */
     @GET("crm/v3/objects/companies")
     Call<CompaniesByPaging> listCompanies(
             @Query("limit") Integer limit,
@@ -20,16 +44,52 @@ public interface CrmApi {
             @Query("archived") Boolean archived
     );
 
+    /**
+     * <p>
+     *     Create a company with the given properties and return a copy of the object, including the ID.
+     * </p>
+     *
+     * @param companyToCreate   A custom request object {@link CompanyRequest}, specifying the properties of the to created company.
+     * @return                  The just created company {@link Company}.
+     * @see                     <a href="https://developers.hubspot.com/docs/api/crm/companies">Companies</a>
+     * @since                   0.0.1
+     */
     @POST("crm/v3/objects/companies")
     Call<Company> createCompany(
             @Body CompanyRequest companyToCreate
     );
 
+    /**
+     * <p>
+     *     Read an Object identified by {companyId}.
+     * </p>
+     *
+     * @param companyId The ID of the company to read.
+     * @return          The company {@link Company} specified by {@param companyId}.
+     * @see             <a href="https://developers.hubspot.com/docs/api/crm/companies">Companies</a>
+     * @since           0.0.1
+     */
     @GET("crm/v3/objects/companies/{companyId}")
     Call<Company> readCompany(
             @Path("companyId") String companyId
     );
 
+    /**
+     * <p>
+     *     Read an Object identified by {companyId} with optional parameters.
+     *     {companyId} refers to the internal object ID by default, or optionally any unique property value as specified by the idProperty query param.
+     *     Control what is returned via the properties query param.
+     * </p>
+     *
+     * @param companyId     The ID of the company to read.
+     * @param properties    A comma separated list of the properties to be returned in the response. If any of the specified properties are not present on the requested object(s), they will be ignored.
+     * @param associations  A comma separated list of object types to retrieve associated IDs for. If any of the specified associations do not exist, they will be ignored.
+     * @param archived      Whether to return only results that have been archived ({@default false}).
+     * @param idProperty    The name of a property whose values are unique for this object type.
+     * @return              The company {@link Company} specified by {@param companyId}.
+     * @see                 <a href="https://developers.hubspot.com/docs/api/crm/companies">Companies</a>
+     * @since               0.0.1
+     */
     @GET("crm/v3/objects/companies/{companyId}")
     Call<Company> readCompany(
             @Path("companyId") String companyId,
@@ -39,12 +99,32 @@ public interface CrmApi {
             @Query("idProperty") String idProperty
     );
 
+    /**
+     * <p>
+     *     Perform a partial update of an Object identified by {companyId}.
+     *     Provided property values will be overwritten.
+     *     Read-only and non-existent properties will be ignored.
+     *     Properties values can be cleared by passing an empty string.
+     * </p>
+     *
+     * @param companyId         The ID of the company to update.
+     * @param companyToUpdate   A custom request object {@link CompanyRequest}, specifying the properties to update for the company.
+     * @return                  The just updated company {@link Company} specified by {@param companyId}.
+     */
     @PATCH("crm/v3/objects/companies/{companyId}")
     Call<Company> updateCompany(
         @Path("companyId") String companyId,
         @Body CompanyRequest companyToUpdate
     );
 
+    /**
+     * <p>
+     *     Move an Object identified by {companyId} to the recycling bin.
+     * </p>
+     *
+     * @param companyId The ID of the company to archive.
+     * @return          A void response will be returned (HTTP 204: No content).
+     */
     @DELETE("crm/v3/objects/companies/{companyId}")
     Call<Void> archiveCompany(
       @Path("companyId") String companyId
